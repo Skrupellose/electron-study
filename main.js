d4c1e3f16a5d2d8f81ef5461d5d122c42d3ff499
hel <hel@tembin.com>
2022-05-24 21:51:04 +0800

    生命周期

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
 const { app, BrowserWindow } = require ('electron')
-app.whenReady().then(()=>{
-    // 初始化窗口尺寸
-    const mainWin = new BrowserWindow({
+function createWindow(){
+     // 初始化窗口尺寸
+     let mainWin = new BrowserWindow({
         window:600,
         height:500
     })
 
     mainWin.loadFile('index.html')
+    mainWin.webContents.on('did-finish-load',()=>{
+        console.log('3-did-finish-load')
+    })
+    mainWin.webContents.on('dom-ready',()=>{
+        console.log('2-dom-ready')
+    })
     // 通过file菜单关闭窗口和右上角关闭按钮均能触发此钩子
     mainWin.on('close',()=>{
-        console.log('closed...')
+        console.log('4-this window is closed...')
+        // 释放内存
+        mainWin = null
     })
+}
+
+app.on('ready',()=>{
+    console.log('1-ready')
+    createWindow()
 })
+
+
 // 通过右上角关闭按钮触发此钩子
 app.on('window-all-closed', ()=>{
-    console.log('app is closed')
+    console.log('8-window-all-closed')
     app.quit()
+})
+
+app.on('before-quit',()=>{
+    console.log('5-before-quit')
+})
+
+app.on('will-quit',()=>{
+    console.log('6-will-quit')
+})
+
+app.on('quit',()=>{
+    console.log('7-quit')
 })
